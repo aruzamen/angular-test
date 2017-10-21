2e6f9799689c227f069f27a6e3f312ce14408eea
Marcelo Aruzamen <aruzamen@gmail.com>
2017-10-21 09:40:19 -0400

    Added article list component

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ArticleListComponent } from '../article-list/article-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [ArticleListComponent]
 })
 export class ArticleModule { }
